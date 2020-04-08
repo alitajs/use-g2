ece2401f265ec9e59ffac6a7b5224a52744db94d
imhele <work@imhele.com>
2020-04-08 10:45:02 +0800

    chore(init): initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  rules: {
+    'consistent-return': 0,
+    'no-bitwise': 0,
+    'no-unused-expressions': 0,
+  },
+};
